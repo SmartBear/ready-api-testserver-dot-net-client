fd57d6bf3885bedc7d766c103be548b54b79d457
IlyaAvdeev <ilya.avdeev@smartbear.com>
2016-03-28 10:59:35 +0300

    Tiny code rearrange.

--- ready-api-testserver-client/ReadyAPI/TestServer/Client/Execution/RecipeExecutor.cs
+++ ready-api-testserver-client/ReadyAPI/TestServer/Client/Execution/RecipeExecutor.cs
@@ -164,58 +164,54 @@ namespace ReadyAPI.TestServer.Client.Execution
             }
         }
 
-        private static string Base64Encode(string plainText)
-        {
-            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
-            return System.Convert.ToBase64String(plainTextBytes);
-        }
-
         private ProjectResultReport SendFilesForDataSources(TestCase body, ProjectResultReport projectResultReport)
         {
             Configuration configuration = _testServerApi.Configuration;
             string executionId = projectResultReport.ExecutionID;
 
-            // verify the required parameter 'body' is set
             if (body == null)
+            {
                 throw new ApiException(400, "Missing required parameter 'body' when calling ReadyapiApi->AddFile");
+            }
 
-            // verify the required parameter 'executionId' is set
             if (executionId == null)
+            {
                 throw new ApiException(400, "Missing required parameter 'executionId' when calling ReadyapiApi->AddFile");
+            }
             
             var path_ = "/readyapi/executions/{executionId}/files";
 
-            var pathParams = new Dictionary<String, String>();
-            var queryParams = new Dictionary<String, String>();
-            var headerParams = new Dictionary<String, String>(configuration.DefaultHeader);
-            var formParams = new Dictionary<String, String>();
-            var fileParams = new Dictionary<String, FileParameter>();
+            var pathParams = new Dictionary<string, string>();
+            var queryParams = new Dictionary<string, string>();
+            var headerParams = new Dictionary<string, string>(configuration.DefaultHeader);
+            var formParams = new Dictionary<string, string>();
+            var fileParams = new Dictionary<string, FileParameter>();
             Object postBody = null;
 
             string boundary = Guid.NewGuid().ToString().Replace('-', 'A');
-            // to determine the Content-Type header
-            String[] httpContentTypes = new String[] {
+            string[] httpContentTypes = new string[] {
                 "multipart/form-data; boundary=" + boundary
-
             };
-            String httpContentType = configuration.ApiClient.SelectHeaderContentType(httpContentTypes);
+            string httpContentType = configuration.ApiClient.SelectHeaderContentType(httpContentTypes);
 
-            // to determine the Accept header
-            String[] httpHeaderAccepts = new String[] {
+            string[] httpHeaderAccepts = new string[] {
                 "application/json"
             };
-            String httpHeaderAccept = configuration.ApiClient.SelectHeaderAccept(httpHeaderAccepts);
+            string httpHeaderAccept = configuration.ApiClient.SelectHeaderAccept(httpHeaderAccepts);
             if (httpHeaderAccept != null)
+            {
                 headerParams.Add("Accept", httpHeaderAccept);
+            }
 
-            // set "format" to json by default
-            // e.g. /pet/{petId}.{format} becomes /pet/{petId}.json
             pathParams.Add("format", "json");
-            if (executionId != null) pathParams.Add("executionId", configuration.ApiClient.ParameterToString(executionId)); // path parameter
+            if (executionId != null)
+            {
+                pathParams.Add("executionId", configuration.ApiClient.ParameterToString(executionId));
+            }
 
             if (_async != null)
             {
-                queryParams.Add("async", configuration.ApiClient.ParameterToString(_async)); // query parameter
+                queryParams.Add("async", configuration.ApiClient.ParameterToString(_async));
             }
 
             Dictionary<string, string> filesToSend = BuildFormParameters(body);
@@ -225,33 +221,17 @@ namespace ReadyAPI.TestServer.Client.Execution
             {
                 string fileName = item.Key;
                 byte [] fileContent = File.ReadAllBytes(item.Value);
-                postBodyStr = "\n--" + boundary + "\nContent-Type: application/octet-stream\nContent-Disposition: form-data; filename=\"" + fileName + "\"; modification-date=\"Wed, 16 Oct 2013 12:54:29 GMT\"; size=10967; name=\"" + fileName + "\"\n\n";
+                postBodyStr = "\n--" + boundary + "\nContent-Type: application/octet-stream\nContent-Disposition: form-data; filename=\"" + fileName + "\"; name=\"" + fileName + "\"\n\n";
                 resultArray = resultArray.Concat(EncodeString(postBodyStr)).ToArray();
                 resultArray = resultArray.Concat(fileContent).ToArray();
                 resultArray = resultArray.Concat(EncodeString("\n--" + boundary + "--")).ToArray();
             }
 
-            /*
-            if (body.GetType() != typeof(byte[]))
-            {
-                postBody = configuration.ApiClient.Serialize(body); // http body (model) parameter
-            }
-            else
-            {
-                postBody = body; // byte array
-            }
-            */
-
-            // authentication (basicAuth) required
-
-            // http basic authentication required
             if (!String.IsNullOrEmpty(configuration.Username) || !String.IsNullOrEmpty(configuration.Password))
             {
                 headerParams["Authorization"] = "Basic " + Base64Encode(configuration.Username + ":" + configuration.Password);
             }
 
-
-            // make the HTTP request
             IRestResponse response = (IRestResponse)configuration.ApiClient.CallApi(path_,
                 Method.POST, queryParams, resultArray, headerParams, formParams, fileParams,
                 pathParams, httpContentType);
@@ -260,44 +240,17 @@ namespace ReadyAPI.TestServer.Client.Execution
             int statusCode = (int)response.StatusCode;
 
             if (statusCode >= 400)
+            {
                 throw new ApiException(statusCode, "Error calling AddFile: " + response.Content, response.Content);
+            }
             else if (statusCode == 0)
+            {
                 throw new ApiException(statusCode, "Error calling AddFile: " + response.ErrorMessage, response.ErrorMessage);
+            }
 
             return new ApiResponse<ProjectResultReport>(statusCode,
                 response.Headers.ToDictionary(x => x.Name, x => x.Value.ToString()),
                 (ProjectResultReport)configuration.ApiClient.Deserialize(response, typeof(ProjectResultReport))).Data;
-
-
-
-            /*            Dictionary<string, string> formParams = BuildFormParameters(body);
-                        if (formParams.Count == 0)
-                        {
-                            return projectResultReport;
-                        }
-                        FormDataMultiPart formDataMultiPart = new FormDataMultiPart();
-                        formDataMultiPart.MediaType = new MediaType();
-                        formDataMultiPart.MediaType.Type = "application";
-                        formDataMultiPart.MediaType.Subtype = "octet-stream";
-                        List<BodyPart> bodyParts = new List<BodyPart>();
-                        foreach (KeyValuePair<string, string> item in formParams)
-                        {
-                            BodyPart bodyPart = new BodyPart();
-                            bodyPart.ContentDisposition = new ContentDisposition();
-                            bodyPart.MediaType = new MediaType();
-
-                            bodyPart.MediaType.Type = "application";
-                            bodyPart.MediaType.Subtype = "octet-stream";
-
-                            bodyPart.ContentDisposition.FileName = item.Key;
-
-                            bodyPart.Entity = item.Value;
-
-                            bodyParts.Add(bodyPart);
-                        }
-                        formDataMultiPart.BodyParts = bodyParts;
-                        return _testServerApi.AddFile(formDataMultiPart, projectResultReport.ExecutionID);
-                        */
         }
 
         static byte[] EncodeString(string input)
@@ -401,6 +354,12 @@ namespace ReadyAPI.TestServer.Client.Execution
                     errorCount++;
                 }
             }
+        }
+
+        private static string Base64Encode(string plainText)
+        {
+            var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(plainText);
+            return System.Convert.ToBase64String(plainTextBytes);
         }
     }
 }
