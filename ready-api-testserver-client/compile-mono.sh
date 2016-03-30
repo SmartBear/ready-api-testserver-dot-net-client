a9087446b29a62bbb20d2c0bd6a64cc7e60a0c66
IlyaAvdeev <ilya.avdeev@smartbear.com>
2016-03-30 10:57:50 +0300

    Fixes sh file

--- ready-api-testserver-client/compile-mono.sh
+++ ready-api-testserver-client/compile-mono.sh
@@ -2,12 +2,13 @@
 mkdir -p bin;
 mcs -sdk:45 -r:../io-swagger-client/bin/Newtonsoft.Json.dll,\
 ../io-swagger-client/bin/RestSharp.dll,\
+../io-swagger-client/bin/IO.Swagger.dll,\
 System.Runtime.Serialization.dll, \
 System.ServiceModel.dll, \
 System.dll, \ 
 System.Core.dll \
 -target:library \
 -out:bin/ReadyAPI.TestServer.Client.dll \
--recurse:'src/*.cs' \
+-recurse:'./*.cs' \
 -doc:bin/ReadyAPI.TestServer.Client.xml \
 -platform:anycpu
